04b72fef86a68deba42df70eee8737ccaa5eed0a
georgie-saunders <georgie.s2@hotmail.com>
2022-05-18 15:28:14 +1000

    file changes

---
+++ scr/script.js
@@ -0,0 +1,8 @@
+let name = prompt("Hey there! What is your name?");
+{
+  alert(
+    "Hey " +
+      name +
+      ", welcome to my website! This is where I'm practicing everything I've learnt in my dev course."
+  );
+}
