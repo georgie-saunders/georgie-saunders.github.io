0d6481d542aeea4442d46f635bb2bb42a6ffcb8b
georgie-saunders <georgie.s2@hotmail.com>
2022-06-10 09:00:06 +1000

    change to weather

--- scr/script.js
+++
@@ -1,8 +0,0 @@
-let name = prompt("Hey there! What is your name?");
-{
-  alert(
-    "Hey " +
-      name +
-      ", welcome to my website! This is where I'm practicing everything I've learnt in my dev course."
-  );
-}
